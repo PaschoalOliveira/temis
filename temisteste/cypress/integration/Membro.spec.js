8f13c16e8e8f2d61524b7ed0902afae7f180c42b
Philipe Santos <philipe_02@hotmail.com>
2020-12-14 11:28:07 -0300

    Teste de membro por id

--- temisteste/cypress/integration/Membro.spec.js
+++ temisteste/cypress/integration/Membro.spec.js
@@ -10,10 +10,25 @@ describe("Teste de Membros", ()=>{
             var achou = false;
             console.log(body);
             body.forEach((elemento) =>{
-                 if(elemento.nome == "joao"){
+                if(elemento.nome == "joao"){
                     achou = true;
-                 }
+                }
             })
+            expect(achou).to.be.equal(true);
+        })
+    })
+    
+    it("GET Membros por ID",() => {
+        cy.request({
+            method:'GET',
+            url:"https://localhost:5001/Membro/2"
+        }).then((response)=>{
+            const body = response.body;
+            var achou = false;
+            console.log(body);
+            if(elemento.membroId == 2){
+                achou = true;
+            }
             expect(achou).to.be.equal(true);
         })
     })
