cbc96cde5a55d216d7c5576433e5a2b4e55fc063
Heverton Reis <heverton.reis04@gmail.com>
2020-12-15 13:13:56 -0300

    Ajustando Arquivos

--- temisteste/cypress/integration/DAJE/steps.js
+++ temisteste/cypress/integration/DAJE/steps.js
@@ -1,15 +1,9 @@
 import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
- 
-<<<<<<< HEAD
-/*Given('Desejo emitir um DAJE', () => {
-	return true;
-});*/
-=======
+
 Given('Desejo emitir um DAJE', () => {
     console.log("cinco");
     return true;
 });
->>>>>>> 25742f32ed83548a7df6445e35d88d8611393428
 
 And('preencher dados do "([^"]*)"$', (args1) => {
 	console.log(args1);
