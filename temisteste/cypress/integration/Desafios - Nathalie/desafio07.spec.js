9a0223cc8f35193219812fb2c2c21acfafb064e7
nathalieSantos <nathalie.santos@solutis.com.br>
2020-12-21 13:38:57 -0300

    Desafio 7 finalizado

--- temisteste/cypress/integration/Desafios - Nathalie/desafio07.spec.js
+++ temisteste/cypress/integration/Desafios - Nathalie/desafio07.spec.js
@@ -3,10 +3,12 @@
 describe("Verifique se o segundo elemento é Chá. Faça o translate da página", ()=>{ 
     it("GET Elemento dois é Chá",() => {
         cy.visit('http://127.0.0.1:5500/temis/temisFront/Desafios/Desafio07.html')
+        .get('button[type="button"]')
+        .click()
         //traduzir e mudar Tea para chá
         cy.get('li')
         .first()
         .next()
-        .should('exist', 'Tea')
+        .contains('Chá')
     })
 })
