f32a7bdb376b839ce32dc166486ffe54bac31d9a
Elayne <elayne.natalia@outlook.com>
2020-12-21 14:51:37 -0300

    Update Startup Startup Jwt

--- temisnet/temis.api/Startup.cs
+++ temisnet/temis.api/Startup.cs
@@ -66,6 +66,26 @@ namespace temis.api
                         Url = new Uri("https://github.com/PaschoalOliveira/temis/tree/feature/dotnet"),
                     }
                 });
+                
+                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer" }
+                    }, new List<string>() }
+                 });
 
             });
 
